8d7f49f491dc628cb7f4bea18ee5b179f894f132
Allan Souza <allan.souza@saude.gov.br>
2022-03-25 11:19:04 -0300

    Troca do delimitador em resultados no formato CSV para ponto e vírgula

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 def data_to_string(value):
+    delimiter = ';'
+
     def __prepare_csv_value(value):
-        return ''.join(('"', value.replace(',',''), '"')) if type(value) is str else str(value)
+        return ''.join(('"', value.replace(delimiter,''), '"')) if type(value) is str else str(value)
 
     def __instance_to_list(_data):
         columns = []
@@ -20,7 +22,7 @@ def data_to_string(value):
         return header + rows
 
     def __to_string(_data):
-        return '\r\n'.join([','.join(i) for i in _data])
+        return '\r\n'.join([delimiter.join(i) for i in _data])
 
     if type(value) is dict:
         qty_keys = len(value.keys())
