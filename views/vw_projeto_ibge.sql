e2639f223e66629cd4e1db69066e077a14b7e4ff
Allan Souza <allan.souza@saude.gov.br>
2022-06-01 05:12:28 -0300

    Inclusão do campo co_st_parecer na view de projetos

--- views/vw_projeto_ibge.sql
+++ views/vw_projeto_ibge.sql
@@ -1,7 +1,7 @@
 create view dbplataformabr.vw_projeto_ibge as
 select
+	co_seq_projeto,
 	replace(tp.nu_caae, '.', '') nu_caae,
-	tp.nu_caae nu_caae_fmt,
 	tp.ds_titulo_publico,
 	tp.no_comite_etica,
 	ti.cidade municipio_comite_etica,
@@ -10,13 +10,22 @@ select
 	tp.co_seq_parecer,
 	tp.dt_submissao,
 	tp.dt_parecer,
-	tp.st_parecer,
+	case lower(st_parecer)
+		when 'aprovado' then 1
+		when 'aprovado com recomendacão' then 2
+		when 'devolvido com recomendacão' then 3
+		when 'não aprovado' then 4
+		when 'pendente' then 5
+		when 'retirado' then 6
+		else null
+	end co_st_parecer,
+	tp.st_parecer,	
 	tp.no_instituicao,
 	ti2.cidade municipio_instituicao,
-	ti2.uf uf_instituicao
+	sg_uf_instituicao
 from
 	dbplataformabr.tb_projeto as tp
 left join
 	dbgeral.tb_ibge as ti on cast(tp.co_municipio_ibge_cep as int) = ti.ibge
 left join
-	dbgeral.tb_ibge as ti2 on cast(tp.co_municipio_ibge_instituicao as int) = ti.ibge;
+	dbgeral.tb_ibge as ti2 on cast(tp.co_municipio_ibge_instituicao as int) = ti2.ibge;
